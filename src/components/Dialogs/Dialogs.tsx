0bdb4fa012a44f59714953b3911d327354dca821
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2024-04-24 17:44:52 +0300

    add dialog component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,4 +1,20 @@
 import s from "./Dialogs.module.css";
 export function Dialogs() {
-  return <div className={s.container}>Dialogs</div>;
+  return (
+    <div className={s.container}>
+      <h2>Dialogs</h2>
+      <div className={s.dialogsContainer}>
+        <div className={s.dialogs}>
+          <div className={s.dialog}>Dmitriy</div>
+          <div className={s.dialog}>Andrey</div>
+          <div className={s.dialog}>Igor</div>
+          <div className={s.dialog}>Sveta</div>
+        </div>
+        <div className={s.messages}>
+          <div className="message">Hi</div>
+          <div className="message">How are you?</div>
+        </div>
+      </div>
+    </div>
+  );
 }
