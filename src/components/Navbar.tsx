869797f30c84a9eed0c2d7b78c94203a708d8ecb
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2024-04-24 10:11:22 +0300

    fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
-import "./Navbar.css";
+import s from "./Navbar.module.css";
 export function Navbar() {
   return (
-    <nav>
+    <nav className={s.nav}>
       <ul>
-        <li>Profile</li>
-        <li>Messages</li>
-        <li>News</li>
-        <li>Music</li>
-        <li>Settings</li>
+        <li className={`${s.item} ${s.active}`}>Profile</li>
+        <li className={s.item}>Messages</li>
+        <li className={s.item}>News</li>
+        <li className={s.item}>Music</li>
+        <li className={s.item}>Settings</li>
       </ul>
     </nav>
   );
